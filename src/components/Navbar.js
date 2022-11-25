2211d98c024d1b997e69376bd9b9f2136264e02d
unknown <m.gouillardon@codeur.online>
2022-11-25 16:43:19 +0100

    feat: add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+export default `
+<nav class="navbar">
+      <div class="navbar__name">Portfolio 2022</div>
+      <ul class="navbar__list">
+        <li class="navbar__item"><a href="#">Home</a></li>
+        <li class="navbar__item"><a href="#work">Work</a></li>
+        <li class="navbar__item"><a href="#about">About</a></li>
+        <li class="navbar__item"><a href="#contact">Contact</a></li>
+      </ul>
+</nav>
+`;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,20 @@ export default {
   render: async () => {
     return `
 
-    <section class="homepage" id="home">
-        
+    <section class="home" id="home">
+      <div class="home__left-side">
+        <h1 class="home__title">
+          <span>Maxime</span><br /><span>Gouillardon</span>
+        </h1>
+      </div>
+      <div class="home__right-side"></div>
     </section>
+    
     `;
   },
   after_render: async () => {
-    
+    let navbar = await import("../components/Navbar");
+    let homepage = document.getElementById("home");
+    homepage.innerHTML += navbar.default;
   },
 };
