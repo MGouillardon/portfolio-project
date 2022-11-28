f19aaf2112cc913a4d54af8ae3e69a2f1af81fc1
Maxime Gouillardon <maxime.gouillardon@gmail.com>
2022-11-28 19:17:46 +0100

    feat: add wrapper homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,23 +3,45 @@ export default {
     return `
 
     <section class="home" id="home">
-  
-      <div class="home__left-side">
-        <h1 class="home__title">
-          <span>Maxime</span><br /><span>Gouillardon</span>
-        </h1>
-        <p class="home__desc">Full stack Web Developer</p>
+    <div class="home__left-side">
+      <h1 class="home__title">
+        <span>Maxime</span><br /><span>Gouillardon</span>
+      </h1>
+      <p class="home__desc">Full stack Web Developer</p>
+    </div>
+    <div class="home__right-side">
+      <div
+        class="wrapper"
+        id="wrapper-js"
+        data-configuration="1"
+        data-roundness="1"
+      >
+        <div class="wrapper__shape"></div>
+        <div class="wrapper__shape"></div>
+        <div class="wrapper__shape"></div>
+        <div class="wrapper__shape"></div>
+        <div class="wrapper__shape"></div>
+        <div class="wrapper__shape"></div>
+        <div class="wrapper__shape"></div>
       </div>
-      <div class="home__right-side"></div>
-      <footer class="home__footer" id="home__footer-js">
-        <ul class="home__footer__list">
-          <li class="home__footer__item">Based in Dijon</li>
-          <li class="home__footer__item">Available to work</li>
-          <li class="home__footer__item"><a href="mailto:maxime.gouillardon@gmail.com" target="_blank">maxime.gouillardon@gmail.com</a></li>
-          <li class="home__footer__item"><a href="http://github.com/MGouillardon" target="_blank">https://github.com/MGouillardon</a></li>
-        </ul>
-      </footer>
-    </section>
+    </div>
+    <footer class="home__footer" id="home__footer-js">
+      <ul class="home__footer__list">
+        <li class="home__footer__item">Based in Dijon</li>
+        <li class="home__footer__item">Available to work</li>
+        <li class="home__footer__item">
+          <a href="mailto:maxime.gouillardon@gmail.com" target="_blank"
+            >maxime.gouillardon@gmail.com</a
+          >
+        </li>
+        <li class="home__footer__item">
+          <a href="http://github.com/MGouillardon" target="_blank"
+            >https://github.com/MGouillardon</a
+          >
+        </li>
+      </ul>
+    </footer>
+  </section>
 
     `;
   },
@@ -32,6 +54,10 @@ export default {
     let curtain__menu__animation = await import("../scripts/curtain__menu__animation");
     let toggleCurtainMenu = curtain__menu__animation.default;
     toggleCurtainMenu();
+    let wrapper__animation = await import("../scripts/wrapper__animation");
+    console.log(wrapper__animation);
+    let wrapperAnimation = wrapper__animation.default;
+    wrapperAnimation();
 
   },
 };
