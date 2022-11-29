fa77fe8d3d84a1ac393a65b9e3d3889c8321433c
unknown <m.gouillardon@codeur.online>
2022-11-29 08:56:17 +0100

    feat: add components navbar into all pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,28 @@
 export default {
-    render: async () => {
-      return `
+  render: async () => {
+    return `
   
       <section class="about" id="about">
           
       </section>
       `;
-    },
-    after_render: async () => {
-      
-    },
-  };
-  
+  },
+  after_render: async () => {
+    let navbar = await import("../components/Navbar");
+    let curtain__menu = await import("../components/Curtain__menu");
+    let about = document.getElementById("about");
+    about.innerHTML += navbar.default;
+    about.innerHTML += curtain__menu.default;
+    // let curtain__menu__animation = await import("../scripts/curtain__menu__animation");
+    // let toggleCurtainMenu =  curtain__menu__animation.default;
+    // toggleCurtainMenu();
+    let btn = document.getElementById("burger-btn");
+    let menu__list = document.getElementById("menu__list-js");
+    let menu__overlay = document.getElementById("menu__overlay-js");
+    btn.addEventListener("click", () => {
+      btn.classList.toggle("on");
+      menu__list.classList.toggle("on");
+      menu__overlay.classList.toggle("on");
+    });
+  },
+};
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,8 +3,8 @@ export default {
     return `     
     <section class="contact" id="contact">
       <div class="contact__content">
-        <div class="contact__content__text">Send me a message</div>
-        <div class="contact__content__mail">maxime.gouillardon@gmail.com</div>
+        <p class="contact__content__text">Send me a message</p>
+        <h1 class="contact__content__mail">maxime.gouillardon@gmail.com</h1>
         <div class="contact__content__links">
           Or you can find me on <a href="https://www.linkedin.com/in/maxime-gouillardon-170833176/" target="_blank">LinkedIn</a>,
           <a href="https://github.com/MGouillardon" target="_blank">Github</a>
